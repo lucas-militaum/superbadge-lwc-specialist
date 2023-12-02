d06358bae84b0bece5f4398dce5f84118bfc0c25
Lucas Militão <lucasmilitao1995@gmail.com>
2023-12-02 10:07:14 -0300

    Atualização por linha na datatable funcional

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,9 +1,10 @@
+// imports
 import { LightningElement } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 
- // imports
  export default class BoatSearch extends NavigationMixin(LightningElement) {
     isLoading = false;
+    boatTypeId = '';
     
     // Handles loading event
     handleLoading() { }
@@ -13,7 +14,9 @@ import { NavigationMixin } from 'lightning/navigation';
     
     // Handles search boat event
     // This custom event comes from the form
-    searchBoats(event) { }
+    searchBoats(event) { 
+      this.boatTypeId = event.detail.boatTypeId;
+    }
     
     createNewBoat() { }
   }
--- force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
+++ force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
@@ -1,9 +1,12 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { publish, MessageContext } from 'lightning/messageService';
 
 import getBoats from '@salesforce/apex/BoatDataService.getBoats';
 import updateBoatList from '@salesforce/apex/BoatDataService.updateBoatList';
+
+import BOATMC from '@salesforce/messageChannel/boatMessageChannel__c'
 
 const SUCCESS_TITLE = 'Success';
 const MESSAGE_SHIP_IT     = 'Ship it!';
@@ -25,11 +28,14 @@ export default class BoatSearchResults extends LightningElement {
     { label: 'Price', fieldName: 'Price__c', type: 'currency', editable: true },
     { label: 'Description', fieldName: 'Description__c', type: 'text', editable: true }
   ];
-  boatTypeId = '';
+  @api boatTypeId = '';
   boats;
   isLoading = false;
+  //my variables
+  draftValues = []
   
   // wired message context
+  @wire(MessageContext)
   messageContext;
   // wired getBoats method
   @wire(getBoats, { boatTypeId: "$boatTypeId" })
@@ -44,18 +50,22 @@ export default class BoatSearchResults extends LightningElement {
   // this public function must refresh the boats asynchronously
   // uses notifyLoading
   async refresh() {
-    this.isLoading(true);
+    this.notifyLoading(true);
     await refreshApex(this.boats);
+    this.draftValues = [];
   }
   
   // this function must update selectedBoatId and call sendMessageService
   updateSelectedTile(event) {
     this.selectedBoatId = event.detail.boatId;
+    this.sendMessageService(this.selectedBoatId);
   }
   
   // Publishes the selected boat Id on the BoatMC.
   sendMessageService(boatId) { 
     // explicitly pass boatId to the parameter recordId
+    const payload = { recordId: boatId };
+    publish(this.messageContext, BOATMC, payload);
   }
   
   // The handleSave method must save the changes in the Boat Editor
@@ -70,24 +80,26 @@ export default class BoatSearchResults extends LightningElement {
     updateBoatList({data: updatedFields})
     .then(() => {
         this.refresh();
-        this.showToast(SUCCESS_TITLE, MESSAGE_SHIP_IT);
+        this.showToast(SUCCESS_TITLE, MESSAGE_SHIP_IT, SUCCESS_VARIANT);
     })
-    .catch(error => { console.error(error); this.showToast(ERROR_TITLE, ERROR_VARIANT); })
-    .finally(() => {this.isLoading(false);});
+    .catch(error => { console.error(error); this.showToast(ERROR_TITLE, error, ERROR_VARIANT); })
+    .finally(() => {this.notifyLoading(false);});
   }
   // Check the current value of isLoading before dispatching the doneloading or loading custom event
   notifyLoading(isLoading) {
     this.isLoading = isLoading;
     if (isLoading) {
       this.dispatchEvent(new CustomEvent('loading'));
+    } else {
+      this.dispatchEvent(new CustomEvent('doneloading'));
     }
-    this.dispatchEvent(new CustomEvent('doneloading'));
   }
 
-  showToast(title, message) {
+  showToast(title, message, variant) {
     const toastEvent = new ShowToastEvent({
         title: title,
-        message: message
+        message: message,
+        variant: variant
     });
     this.dispatchEvent(toastEvent);
   }
